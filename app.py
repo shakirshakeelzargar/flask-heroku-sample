0ad39f8673cebb50c75c690b799245a156a9cee7
Shakir Shakeel <shakir.s@softcrylic.co.in>
2020-09-23 04:20:21 +0800

    ss

--- app.py
+++ app.py
@@ -8,9 +8,25 @@ app = Flask(__name__)
 
 
 
+
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html')
+  try:
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.keys import Keys
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = "/app/.apt/usr/bin/google_chrome"
+    driver=webdriver.Chrome("/app/.chromedriver/bin/chromedriver")
+    driver.get("http://www.naukri.com")
+    return render_template('index.html')
+  except Exception as ex:
+    return render_template('error.html',err=str(ex))
 
 
 
