48c98d0741f3ab7aaf3b282af910e414cf516bd6
Shakir Shakeel <shakir.s@softcrylic.co.in>
2020-09-23 12:50:46 +0530

    all

--- app.py
+++ app.py
@@ -24,6 +24,22 @@ def index():
     chrome_options.binary_location = "/app/.apt/usr/bin/google_chrome"
     driver=webdriver.Chrome("/app/.chromedriver/bin/chromedriver")
     driver.get("http://www.naukri.com")
+    import time
+    time.sleep(3)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//a[@title='Jobseeker Login']"))).click()
+    time.sleep(3)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Enter your active Email ID / Username']"))).send_keys("shakirshakeelzargar11@gmail.com")
+    time.sleep(3)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Enter your password']"))).send_keys("Shakir@11")
+    time.sleep(3)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[@class='btn-primary loginButton']"))).click()
+    time.sleep(3)
+    driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid")
+    time.sleep(3)
+    up_btn=WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@id='attachCV']")))
+    time.sleep(3)
+    up_btn.send_keys("Resume_Shakir_July2020_public")
+    time.sleep(3)
     return render_template('index.html')
   except Exception as ex:
     return render_template('error.html',err="There was an error: "+str(ex))
